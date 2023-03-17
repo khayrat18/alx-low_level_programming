17df4223483b6936ff5d4ec87db326db213290ac
khayrat18 <hairatua@yahoo.com>
2023-03-17 16:26:03 -0700

    c varaiables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Main entry
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+char ch = 'a';
+char i = 'A'
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (i = 'A'; i <= 'Z'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
