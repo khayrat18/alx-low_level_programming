500cc5d3f47448342def6f3cbb88bde7d34b8ee6
khayrat18 <hairatua@yahoo.com>
2023-03-17 18:45:34 -0700

    v variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Main entry
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+char ch = 'z';
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+putchar('\n');
+
+return (0);
+}
