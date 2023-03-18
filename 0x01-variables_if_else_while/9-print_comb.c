7a078f04635f294e2b0fac5c74888212117d8697
khayrat18 <hairatua@yahoo.com>
2023-03-17 20:14:05 -0700

    c variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,14 @@ int i;
 
 for (i = 0; i < 10; i++)
 {
-if (i == 9)
 putchar(i + '0');
-else
+if (i != 9)
 {
-putchar(i + '0');
 putchar(',');
 putchar(' ');
 }
 }
+putchar('\n');
 return (0);
 }
 
