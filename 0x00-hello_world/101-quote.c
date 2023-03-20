51c8b3b7fb62ff18222a6fb3884cf5783e81b7ae
khayrat18 <hairatua@yahoo.com>
2023-03-20 03:46:45 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+
+putchar(and that piece of art is useful" - Dora Korpar, 2015-10-19);
+putchar('\n');
+
+Return (1);
+}
+
