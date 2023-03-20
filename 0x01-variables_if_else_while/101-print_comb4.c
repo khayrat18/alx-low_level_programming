cfe4197655299b23ca4ddc095ff4cc200315ffab
khayrat18 <hairatua@yahoo.com>
2023-03-20 05:06:36 -0700

    c variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,19 +4,27 @@
  *
  * Return: returns 0
  */
+
 int main(void)
 {
-int i, j;
+int i, j, k;
 for (i = 0; i < 10; i++)
 {
 for (j = i + 1; j < 10; j++)
 {
+for (k = j + 1; k < 10; k++)
+{
+if (i != j && i != k && j != k)
+{
 putchar(i + '0');
 putchar(j + '0');
-if (i < 8 || j < 9)
+putchar(k + '0');
+if (i < 7 || j < 8 || k < 9)
 {
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 }
