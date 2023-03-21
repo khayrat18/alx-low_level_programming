28ab21e0bf2c0ed15539b6e6a07f7bf46fed776f
khayrat18 <hairatua@yahoo.com>
2023-03-21 05:08:59 -0700

    c variables

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>i
+#include <main.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+}
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
