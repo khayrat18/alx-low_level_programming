9ce1e3cb8f003dfffb44046ff5d08b0434705f9f
khayrat18 <hairatua@yahoo.com>
2023-03-22 13:46:20 -0700

    c functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,13 @@
 */
 int print_last_digit(int n)
 {
-int a;
-if (n < 0)
-n = -n;
-a = n % 10;
+	int a;
 
-if (a < 0);
-a = -a;
-_putchar(a + '0');
-return (a);
+	a = n % 10;
+	if (n < 0)
+	a = -a;
+	_putchar(a + '0');
+	return (a);
+
+
 }
