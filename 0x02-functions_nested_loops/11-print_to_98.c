86c6364737d7a81aa9f24ca4013ae75060780e6f
khayrat18 <hairatua@yahoo.com>
2023-03-22 06:49:03 -0700

    c functions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_to_98 - print numbers n to 98
+ * @n: numbers to be printed
+ * Return: Always o
+ */
+void print_to_98(int n)
+{
+for (n = 0; n <= 98; n++)
+_putchar(',');
+_putchar(' ');
+_putchar('\n');
+}
