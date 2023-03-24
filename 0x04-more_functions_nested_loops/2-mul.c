8a4801f7d8e1d9ec94b6839cc0ed4a8f984d197c
khayrat18 <hairatua@yahoo.com>
2023-03-24 02:38:16 -0700

    more c functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - checks for the result of two  characters
+ *
+ * @a: first character to be checked
+ * @b: second number to be checked
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+
+int c = a * b;
+return (c);
+}
