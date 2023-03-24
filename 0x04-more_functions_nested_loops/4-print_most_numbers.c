94d52b5ca1c7656d2229b47b29ce958179ad92fb
khayrat18 <hairatua@yahoo.com>
2023-03-24 03:40:04 -0700

    more c functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_most_numbers - checks for uppercase character
+ *
+ * Return: always 0
+ */
+void print_most_numbers(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+if (!(c == '2' || c == '4'))
+_putchar(c);
+}
+
+
+
+
+
+
+}
