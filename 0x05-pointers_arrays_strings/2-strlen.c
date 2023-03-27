48ad33201acca0abd5077512fa16156946a27a83
khayrat18 <hairatua@yahoo.com>
2023-03-27 06:36:35 -0700

    c pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * _strlen  - checks for length of a string
+ *
+ * @s: string to be checked
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+
+int len;
+
+while (*s != '\0')
+{
+len++;
+s++;
+
+}
+return (0);
+}
