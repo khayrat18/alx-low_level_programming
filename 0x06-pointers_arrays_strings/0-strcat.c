d89984d18858fe1c5c155ba21e170225204407ad
khayrat18 <hairatua@yahoo.com>
2023-03-29 03:34:36 -0700

    more c pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - function that concatenates two strings
+ *
+ *@dest: string to join with
+ *@src: string to join with first string
+ * Return: Return dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
