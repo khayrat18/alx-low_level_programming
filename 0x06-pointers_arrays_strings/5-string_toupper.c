329c31028158acbc093e5725d776978123de0216
khayrat18 <hairatua@yahoo.com>
2023-03-30 03:46:29 -0700

    more c pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,11 +7,13 @@
  */
 char *string_toupper(char *a)
 {
-int i = 0;
+int i;
+i = 0;
 while (a[i] != '\0')
 {
 if (a[i] >= 'a' && a[i] <= 'z')
-a[i] = a[1] - 32;
+a[i] = a[i] - 32;
+i++;
 }
 return (a);
 }
