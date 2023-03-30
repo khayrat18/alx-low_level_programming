adcc707f7eead6e12efd15b1ac5ac3f1f50e4c3f
khayrat18 <hairatua@yahoo.com>
2023-03-30 04:13:52 -0700

    more c pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,17 +11,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 == *s2)
+int i;
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
 {
-if (*s1 == '\0' || *s2 == '\0')
-break;
-s1++;
-s2++;
+if (s1[i] != s2[i])
 
+return (s1[i] - s2[i]);
+
+i++;
 }
-if (*s1 == '\0' && *s2 == '\0')
 return (0);
-else 
-return (-1);
 
 }
