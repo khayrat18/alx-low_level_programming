d68d6b1ce4b299ef09976acfada4eb2f15e45d07
khayrat18 <hairatua@yahoo.com>
2023-04-03 09:13:06 -0700

    even more c pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,20 +11,13 @@
 
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
-if (*s == c)
+char *z;
+z = strchr(s, c);
+if (z != 0)
 {
-return (s);
-}
-s++;
-if (c == '\0')
-{
-
-return (s);
+return (z);
 
 }
-
-
+else
 return (NULL);
-
 }
