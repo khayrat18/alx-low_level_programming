2ea0025e844ac5f6136cb42655542c04f96a1d17
khayrat18 <hairatua@yahoo.com>
2023-04-04 03:03:11 -0700

    even more c pointers

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_chessboard - function that prints the chessboard
+ *
+ * @a: string to checked
+ *
+ * Return: always 0
+ */
+
+
+
+
+void print_chessboard(char (*a)[8])
+{
+char **p;
+**p = &a;
+int  x;
+x =0;
+while (a == 0)
+
+	if ( x <8)
+
+print_chessboard (**p);
+x++;
+}
