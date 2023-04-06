282e462cd924be5c6ca900036185109b060cbbb6
khayrat18 <hairatua@yahoo.com>
2023-04-05 18:46:36 -0700

    c recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,6 @@
 #include <math.h>
 
 /**
- *
  * _pow_recursion - returns value of x and y
  *
  * @x: number to be checked
@@ -25,7 +24,7 @@ else if (y < 0)
 return (-1);
 }
 
-return (x*_pow_recursion(x, y -1));
+return (x * _pow_recursion(x, y - 1));
 
 
 
