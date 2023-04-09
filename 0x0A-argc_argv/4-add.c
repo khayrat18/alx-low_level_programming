70b9cfed1aeaf815a0b4d1377743cfee2cf257ec
khayrat18 <hairatua@yahoo.com>
2023-04-08 18:57:54 -0700

    argc and argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,68 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
 
 /**
- * main - adds two positive integers
+ * check_digit - check if string is a digit
+ * @str: string to be checked
+ *
+ * Return: always 0
+ */
+
+int check_digit(char *str)
+{
+int i;
+
+
+for (i = 0; str[i] != '\0'; i++)
+        {
+        if (isdigit(str[i]) == 0)
+        return (0);
+        }
+return (1);
+
+
+}
+
+/**
+ * main - adds two positive numbers
  *
  * @argc: number of arguments
  * @argv: array of arguments
  *
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
-{
 
-if(argc == 0)
 {
-printf("0\n");
-}
 
 int i;
-for(i = 0; i < argc; i++)
-{	
-	printf("Error\n");
+int str_to_digit;
+int sum = 0;
+
+i = 1;
+while (i < argc)
+{
+if (check_digit(argv[i]))
+
+{
+str_to_digit = atoi(argv[i]);
+sum += str_to_digit;
+}
+else
+{
+printf("Error\n");
 return (1);
 }
 
+i++;
+}
 
-
-int a;
-int b;
-int sum;
-a = _atoi(argv[1]);
-b = _atoi(argv[2]);
-sum = a + b;
-printf("%d\n", a,b,sum);
+printf("%d\n", sum);
 
 return (0);
 }
