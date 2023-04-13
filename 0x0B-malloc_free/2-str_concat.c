dd31ee7d8e0e8b3660c6c5455e26c505aac27362
khayrat18 <hairatua@yahoo.com>
2023-04-12 17:40:21 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,12 +15,16 @@ char *str_concat(char *s1, char *s2)
 {
 
 char *s;
+int x;
+int y;
 
 s = malloc(strlen(s1) + strlen(s2) + 1 * sizeof(char));
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (x = 0; s1[x] != '\0'; x++)
+	for (y = 0; s2[y] != '\0'; y++)
 	if (s == NULL)
 	{
 		return (NULL);
