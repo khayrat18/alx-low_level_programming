471888e49ee46660f80d4a79d5f6c2bfbc46bcbe
khayrat18 <hairatua@yahoo.com>
2023-06-19 14:03:06 -0700

    upadated task 0

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdlib.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -25,4 +23,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 
-#endif
+#endif /* _MAIN_H_ */
